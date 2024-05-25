e928f81842805e291087dc9206ff26ca99a676b8
kusstar <kussssss@outlook.com>
2024-05-25 16:08:08 +0800

    feat: gen-list

---
+++ gen-list.ts
@@ -0,0 +1,45 @@
+import { Glob } from "bun";
+
+const glob = new Glob("*");
+
+let md = ""
+
+const getExtLineNumber = async (file: string, extension: {
+  name: string,
+  description: string
+}) => {
+  const lines = (await Bun.file(file).text()).split("\n")
+  let lineNumber = 0
+  for (const line of lines) {
+    if (line.includes(extension.name)) {
+      return lineNumber
+    }
+    lineNumber++
+  }
+  return -1
+}
+
+for (const name of glob.scanSync("./extensions")) {
+  const file = './extensions/' + name
+  const extension = await Bun.file(file).json()
+
+  if (extension.main) {
+    md += `### [${extension.name}](./extensions/${name})\n\n`
+    for (const ext of extension.main) {
+      const lineNumber = await getExtLineNumber(file, ext)
+      md += `- [${ext.name}](./extensions/${name}#L${lineNumber})\n  ${ext.description}\n\n`
+    }
+  }
+}
+
+// console.log(md)
+
+const readme = await Bun.file("./README.md").text()
+
+const anchor = "## List"
+
+const start = readme.indexOf(anchor)
+
+const newReadme = readme.slice(0, start + anchor.length) + "\n\n" + md
+
+Bun.write("./README.md", newReadme)
